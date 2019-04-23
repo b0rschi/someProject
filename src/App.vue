d16cf77f3664484e543db25b1597a9d58ebd3aeb
b0rschi <b0rsch.i@yandex.ru>
2019-04-23 14:52:33 +0300

    todoList as app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id='app' >
+      <form v-on:submit.prevent='todoAddTask'>
+        <label for='todoAdd'></label>
+        <input id='todoAdd' size=50 placeholder="Добавить новую задачу" v-model='todoAddText'>
+        <button>Добавить задачу</button>
+      </form>
+      <ul>
+        <li
+        is='todo-item'
+        v-for='(todo, index) in todoList'
+        v-bind:key='todo.id'
+        v-bind:title='todo.title'
+        v-on:remove='todoList.splice(index, 1)'
+        ></li>
+      </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todoItem from './components/todoItem.vue'
 
 export default {
   name: 'app',
+  data(){
+    return {
+    todoList: [
+          {id: 1, title: 'Добавить функций в todoList'}
+        ],
+        todoAddText: '',
+        todoNextIndex: 2
+    }
+  },
+    methods:{
+        todoAddTask: function(){
+          this.todoList.push({
+            id: this.todoNextIndex++,
+            title: this.todoAddText
+          });
+          this.todoAddText = '';
+        }
+      },
   components: {
-    HelloWorld
+    todoItem
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
